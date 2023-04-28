0a607f5efdcd0b0d472b07b0f8c3bc09cb31b3a5
bryan <rocafueteherrera@gmail.com>
2023-04-27 23:23:47 -0500

    recyclerView listview

--- app/src/main/java/com/example/view/MainActivity.kt
+++ app/src/main/java/com/example/view/MainActivity.kt
@@ -2,10 +2,74 @@ package com.example.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.view.MyAdapter
+import com.example.view.News
+import com.example.view.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var newRecylerview : RecyclerView
+    private lateinit var newArrayList :  ArrayList<News>
+    lateinit var imageId : Array<Int>
+    lateinit var heading : Array<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageId = arrayOf(
+                R.drawable.a,
+                R.drawable.b,
+                R.drawable.c,
+                R.drawable.d,
+                R.drawable.e,
+                R.drawable.f,
+                R.drawable.g,
+                R.drawable.h,
+                R.drawable.i,
+                R.drawable.j
+
+        )
+
+        heading = arrayOf(
+            "Biden aims to expand vaccines for adults and children",
+            "Just got my first shot, helping the world to be a safer place",
+            "Local trains to be suspended in Bengal from tomorrow in view of covid-19",
+            "HA asks states,UTs to ensure there are no fires in hospitals",
+            "Australian citizen sues pM Morrison over flight ban from india",
+            "Former India hockey coach kaushik hospitalised after testing positive for COVID",
+            "Delhi records 20,960 fresh covid-19 infections, positivity rate at 26.37%",
+            "Barcelona church offers open-air space for Ramadan",
+            "Trillions of cicadas set to emerge in the US, here's why",
+            "Homemaker , economist: Cadidates from a the walk her life in Bengal assembly",
+        )
+
+        newRecylerview =findViewById(R.id.recyclerView)
+        newRecylerview.layoutManager = LinearLayoutManager(this)
+        newRecylerview.setHasFixedSize(true)
+
+        newArrayList = arrayListOf<News>()
+        getUserdata()
+
     }
+
+    private fun getUserdata() {
+
+        for (i in imageId.indices) {
+
+
+            val news = News(imageId[i], heading[i])
+            newArrayList.add(news)
+
+
+        }
+
+
+        newRecylerview.adapter = MyAdapter(newArrayList)
+
+
+
+    }
+
 }
